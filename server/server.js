b0c58bd030df50b2bde286408749035728f12f40
Kush Agarwal <agrawalkush783@gmail.com>
2026-05-03 21:43:46 +0530

    fixed express route issue

--- server/server.js
+++ server/server.js
@@ -163,7 +163,9 @@ if (process.env.NODE_ENV === 'production') {
   const clientDist = path.join(__dirname, '../client/dist');
   if (fs.existsSync(clientDist)) {
     app.use(express.static(clientDist));
-    app.get('*', (req, res) => res.sendFile(path.join(clientDist, 'index.html')));
+    app.get('/{*splat}', (req, res) =>
+      res.sendFile(path.join(clientDist, 'index.html'))
+    );
   }
 }
 
